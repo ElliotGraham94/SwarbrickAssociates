c99d9441a70dc0b491e35c8000b7ee134112f691
Elliot Graham <ElliotGraham94@gmail.com>
2016-10-22 13:55:33 +0100

    scrolltofixed.js removed - try https://www.viget.com/articles/jquery-stick-em

--- projects/demoProject/project.js
+++ projects/demoProject/project.js
@@ -1,6 +1,1 @@
-$('#nav').affix({
-    offset: {     
-      top: $('#nav').offset().top -30,
-      bottom: ($('.secondrow').outerHeight(true) + $('.thirdrow').outerHeight(true)) + 40
-    }
-});
+
